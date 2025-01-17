5ead07d2fc4b06f68ac1293922bd9dff35430e92
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2025-01-17 16:03:14 +0900

    TableViewerを追加

--- tmp/lib/hitcounter.ts
+++ tmp/lib/hitcounter.ts
@@ -1,3 +1,4 @@
+import { RemovalPolicy } from "aws-cdk-lib";
 import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Code, Function, IFunction, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
@@ -10,12 +11,18 @@ export class HitCounter extends Construct {
   // api gatewayにhandlerを登録するために、publicに露出する必要がある
   public readonly handler: Function;
 
+  // dynamo table viewerにtableを登録するために、publicに露出する必要がある
+  public readonly table: Table;
+
   constructor(scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
     // テーブルを定義
-    const table = new Table(this, "Hits", {
+    this.table = new Table(this, "Hits", {
       partitionKey: { name: "path", type: AttributeType.STRING },
+      // RETAIN_ON_UPDATE_OR_DELETE: 未使用な場合のみ削除する
+      // SNAPSHOT: 削除するけどスナップショットをとっておく
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
     // handlerを定義
@@ -25,12 +32,12 @@ export class HitCounter extends Construct {
       code: Code.fromAsset("lambda"),
       environment: {
         DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName, // lambdaコードに、cdkのコードから渡す
-        HITS_TABLE_NAME: table.tableName, // lambdaコードに、cdkのコードから渡す
+        HITS_TABLE_NAME: this.table.tableName, // lambdaコードに、cdkのコードから渡す
       },
     });
 
     // handlerがtableに書き込みすることを許可する
-    table.grantReadWriteData(this.handler);
+    this.table.grantReadWriteData(this.handler);
 
     // handlerがdownstreamをinvokeすることを許可する
     props.downstream.grantInvoke(this.handler);
--- tmp/lib/tmp-stack.ts
+++ tmp/lib/tmp-stack.ts
@@ -3,6 +3,7 @@ import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 import { Runtime, Function, Code } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { HitCounter } from "./hitcounter";
+import { TableViewer } from "cdk-dynamo-table-viewer";
 
 export class TmpStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -22,6 +23,12 @@ export class TmpStack extends Stack {
     const gateway = new LambdaRestApi(this, "Endpoint", {
       // 第二引数はリソースの名称。コンソールで表示される
       handler: helloWithCounter.handler,
+    });
+
+    const tv = new TableViewer(this, "ViewHitCounter", {
+      title: "Hello Hits",
+      table: helloWithCounter.table,
+      sortBy: "-hits",
     });
   }
 }
